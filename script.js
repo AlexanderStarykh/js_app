23d1fc0fd98afb0ade781a77e1ba0a087dcc1be2
Alexander Starykh <alexander000starykh@gmail.com>
2022-05-26 18:13:10 +0300

    revert

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 'use strict';
 
-    let money, time;
-
-function start(){
-    time = prompt("Введите дату в формате YYYY-MM-D", "2022-05-26");
-    
-    while (isNaN(money) || money == "" || money == null){
-        money = +prompt("Ваш бюджет на месяц?");
-    }
-}
-
-start();
+let time = prompt("Введите дату в формате YYYY-MM-D", "2022-05-26"),
+    money = +prompt("Ваш бюджет на месяц?");
 
 var appData = {
     budget: money,
@@ -18,22 +9,7 @@ var appData = {
     expenses: {},
     optionalExpenses:{},
     income: [],
-    savings: true
-};
-
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++){
-        let a = prompt("Введите обязательную статью расходов в этом месяце", " "),
-            b = prompt("Во сколько обойдется?", " ");
-        if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) === 'string' && (typeof(b)) != null
-            && a != '' && b != '' && a.length < 50 ) {
-            console.log("done");
-            appData.expenses[a] = b; 
-        } else {
-            i--;
-        };
-           
-    }
+    savings: false
 };
 
 chooseExpenses();
@@ -59,21 +35,29 @@ function detectLevel(){
         console.log("Middle level of budget")
     } else if (appData.moneyPerDay > 2000){
         console.log("You are rich!")
+for (let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", " "),
+        b = prompt("Во сколько обойдется?", " ");
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) === 'string' && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50 ) {
+        console.log("done");
+        appData.expenses[a] = b; 
+
     } else {
-        console.log("Error!")    
+        i--;
     };
-}
+       
+};
 
-detectLevel();
+appData.moneyPerDay = appData.budget/30;
 
-function checkSavings(){
-   if (appData.savings == true){
-       let save = +prompt("Какова сумма накоплений?", ''),
-            percent = +prompt("Which percent?", '');
-
-        appData.monthIncome = ((save/100/12)*percent).toFixed(2);
-        alert("Month income is: " + appData.monthIncome);
-   }
-}
-
-checkSavings();
+alert("Budget per day:" + appData.moneyPerDay);
+if (appData.moneyPerDay < 100){
+    console.log("Minimal level of budget")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Middle level of budget")
+} else if (appData.moneyPerDay > 2000){
+    console.log("You are rich!")
+} else {
+    console.log("Error!")    
+};
